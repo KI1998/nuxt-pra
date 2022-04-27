e22dd4bf0d3023ddd0eb502d5214108b46c77ece
ichikawakoutarou <k_ichikawa@innovation.co.jp>
2022-04-27 16:55:08 +0900

    ルーティングの設定。階層ページを作成。

---
+++ pages/users.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+
+<!--    NuxtChildコンポーネントを利用することで、users/lists.vueファイルの外側にusers.vueファイルに記述した内容を表示させられる-->
+    <h1>I am the parent view</h1>
+    <NuxtChild/>
+  </div>
+</template>
