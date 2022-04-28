80fb2f1445e98483acf80e49b2fd68bb72ddd0f3
ichikawakoutarou <k_ichikawa@innovation.co.jp>
2022-04-28 11:26:56 +0900

    prefetchの設定

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,10 @@
   <div>
     <nav>
       <nuxt-link to="/">Home</nuxt-link>
-      <nuxt-link to="/about" no-prefetch>About</nuxt-link>
+
+<!--   prefetchはviewportに入ったリンクのデータをページにアクセス前に事前に取得できる機能   -->
+      <nuxt-link to="/about" no-prefetch>About page not prefetched</nuxt-link>
+      <nuxt-link to="/about" :prefetch="false">About page not prefetched</nuxt-link>
       <nuxt-link :to="{ name: 'users-list' }">User List</nuxt-link>
     </nav>
   </div>
