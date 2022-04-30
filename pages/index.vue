febae91d5e432faf39bf7d8ffa825dd4360496af
ichikawakoutarou <k_ichikawa@innovation.co.jp>
2022-04-30 15:12:08 +0900

    Vuexの設定

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div>count: {{ $store.state.count }}</div>
+    <button @click="addCount" >Add</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  methods: {
+    addCount() {
+      this.$store.dispatch("increment");
+    }
+  }
+};
 </script>
