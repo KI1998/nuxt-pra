35da3f63d7ef83785989cd8c2bfc57d78480cf48
ichikawakoutarou <k_ichikawa@innovation.co.jp>
2022-05-01 21:37:09 +0900

    Vueのプラグインを利用

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,50 @@
 <template>
   <div>
-    <div>count: {{ $store.state.count.count }}</div>
-    <button @click="addCount" >Add</button>
+    <h1>Pluginの動作確認</h1>
+    <button v-tooltip="'TOPにメッセージが表示される'" >TOP</button>
   </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    addCount() {
-      this.$store.dispatch("count/increment");
-    }
-  }
-};
+export default {};
 </script>
+
+<style>
+.tooltip {
+  display: block !important;
+  padding: 4px;
+  z-index: 10000;
+}
+
+.tooltip .tooltip-inner {
+  background: black;
+  color: white;
+  border-radius: 16px;
+  padding: 5px 10px 4px;
+}
+
+.tooltip .tooltip-arrow {
+  display: none;
+}
+
+.tooltip[aria-hidden="true"] {
+  visibility: hidden;
+  opacity: 0;
+  transition: opacity 0.15s, visibility 0.15s;
+}
+
+.tooltip[aria-hidden="false"] {
+  visibility: visible;
+  opacity: 1;
+  transition: opacity 0.15s;
+}
+
+.tooltip.danger .tooltip-inner {
+  background: red;
+}
+
+.tooltip.caution .tooltip-inner {
+  background: yellow;
+  color: black;
+}
+</style>
